0b79504b74025ed1a95784537967c1fbb5cc8fd4
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-04-09 15:49:57 +0930

    add prompts for telescope key binds

--- .config/nvim/lua/config-telescope/key-binds.lua
+++ .config/nvim/lua/config-telescope/key-binds.lua
@@ -1,5 +1,19 @@
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true, silent = true})
+local which_key = require 'which-key'
 
+local actions = {
+	find_files = '<cmd>Telescope find_files<cr>',
+	live_grep = '<cmd>Telescope live_grep<cr>',
+	find_buffers = '<cmd>Telescope buffers<cr>',
+	help_tags = '<cmd>Telescope help_tags<cr>',
+}
+
+which_key.register({
+	f = {
+		name = 'find',
+
+		f = { actions.find_files, 'files', },
+		g = { actions.live_grep, 'grep', },
+		b = { actions.find_buffers, 'buffers', },
+		h = { actions.help_tags, 'help tags', },
+	}
+}, { prefix = '<leader>' })
