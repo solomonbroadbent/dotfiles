1f7871548fc7eec62ac0f23ce43b209619b9da53
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-04-28 13:50:48 +0930

    add autosuggestions and syntax highlighting back into zsh

---
+++ .config/zsh/plugins/autosuggestions/init.zsh
@@ -0,0 +1,5 @@
+# TODO: setup git submodules
+# install plugin from repo:
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/autosuggestions/autosuggestions
+
+source ~/.config/zsh/plugins/autosuggestions/autosuggestions/zsh-autosuggestions.zsh
---
+++ .config/zsh/plugins/syntax-highlighting/init.zsh
@@ -0,0 +1,5 @@
+# TODO: setup git submodules
+# install plugin from repo:
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/syntax-highlighting/syntax-highlighting
+
+source .config/zsh/plugins/syntax-highlighting/syntax-highlighting/zsh-syntax-highlighting.zsh
