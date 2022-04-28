a8396da5fbe6992b3fba2a8648da73a3d324e902
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-04-28 13:41:08 +0930

    add vi-mode plugin back into zsh

---
+++ .config/zsh/plugins/vi-mode/init.zsh
@@ -0,0 +1,5 @@
+# TODO: setup git submodules
+# install with:
+# git clone https://github.com/jeffreytse/zsh-vi-mode.git ~/.config/zsh/plugins/vi-mode/vi-mode
+
+source ~/.config/zsh/plugins/vi-mode/vi-mode/zsh-vi-mode.plugin.zsh
