352ec25c229afccfd0548e01ba54f879e3a66e2a
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-05-03 16:21:39 +0930

    enable vi-mode and history-reverse-substring-search in zsh

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -2,13 +2,14 @@
 source ~/.config/zsh/tweaks/powerlevel10k-instant-prompt.zsh
 
 # plugins
-source ~/.config/zsh/plugins/vi-mode/init.zsh
+local plugins="~/.config/zsh/plugins"
 source ~/.config/zsh/plugins/autosuggestions/init.zsh
 source ~/.config/zsh/plugins/syntax-highlighting/init.zsh
+source ~/.config/zsh/plugins/history-substring-search/key-binds.zsh
 source ~/.config/zsh/plugins/history-substring-search/init.zsh # ensure loaded —after— syntax highlighting
+source ~/.config/zsh/plugins/vi-mode/init.zsh
 
 # key-binds
-source ~/.config/zsh/plugins/history-substring-search/key-binds.zsh
 
 source ~/.config/zsh/aliases/init.zsh
 source ~/.config/zsh/tweaks/init.zsh
