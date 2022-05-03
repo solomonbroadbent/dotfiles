e563d23df0b197e305f9fe77a4b719b67b2f28ac
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-05-03 17:41:53 +0930

    don't show duplicate commands when cycling through history zsh

--- .config/zsh/plugins/history-substring-search/init.zsh
+++ .config/zsh/plugins/history-substring-search/init.zsh
@@ -3,6 +3,6 @@
 # git clone https://github.com/zsh-users/zsh-history-substring-search ~/.config/zsh/plugins/history-substring-search/history-substring-search
 
 # don't show identical commands when cycling through history
-# setopt HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = true
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=true
 
 source ~/.config/zsh/plugins/history-substring-search/history-substring-search/zsh-history-substring-search.zsh
